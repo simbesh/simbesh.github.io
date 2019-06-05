ac9a0a993e0311e0ecd987e2d8ac95bc144f26b5
Simon <simon.bechard@gmail.com>
2019-06-05 13:07:15 +1000

    Better error displaying
    add mailto link in footer
    css tweaks

--- contact.js
+++ contact.js
@@ -10,6 +10,7 @@ const registerListeners = () => {
     document.getElementById("submit-button").addEventListener("click", async function (event) {
         event.preventDefault()
         document.getElementById("submit-button").setAttribute('disabled', '');
+        document.getElementById("errors").classList.add('d-none');
         let errors = false
         let errorMessage = ""
         const options = `&parse_mode=HTML&disable_web_page_preview=true`
@@ -34,29 +35,32 @@ const registerListeners = () => {
         }
         const ip = localStorage.getItem('clientIp') || "could not retrieve"
         const text = encodeURIComponent(`Contact from bechard.dev!
-First Name:      ${fname}
-Last Name:      ${lname}
-Email:                ${email}
-Message:
+F: ${fname}
+L: ${lname}
+E: ${email}
+M:
 ${message}
-IP: ${ip}
+IP: <code>${ip}</code>
 <a href="https://tools.keycdn.com/geo?host=${ip}">Geolocation info</a>`)
 
         const url = `https://api.telegram.org/bot816925224:AAGkot8xPR1aMOSg8Xowpj_DUjK_A-B9HKU/sendMessage?chat_id=386572077&text=${text}${options}`
-        const res = await fetch(url)
-        console.log('response :', res)
-        if (res.status >= 200 && res.status < 300){
-            document.getElementById("contact-container").classList.add('d-none')
-            document.getElementById("contact-success").classList.remove('d-none')
-        } else {
-            document.getElementById("contact-container").classList.add('d-none')
-            const errorCard = document.getElementById("contact-error")
-            errorCard.classList.remove('d-none')
-            const p = document.createElement('p')
-            const text = document.createTextNode(`Error Code: ${res.status} - ${res.statusText}`)
-            p.appendChild(text)
-            errorCard.appendChild(p)
-
+        let res = null;
+        try {
+            res = await fetch(url)            
+            if (res.status >= 200 && res.status < 300){
+                document.getElementById("contact-container").classList.add('d-none')
+                document.getElementById("contact-success").classList.remove('d-none')
+            } else {
+                document.getElementById("contact-container").classList.add('d-none')
+                const errorCard = document.getElementById("contact-error")
+                errorCard.classList.remove('d-none')
+                const p = document.createElement('p')
+                const text = document.createTextNode(`Error Code: ${res.status} - ${res.statusText}`)
+                p.appendChild(text)
+                errorCard.appendChild(p)
+            }
+        } catch (error) {
+            displayError("Send failed, please try again");
         }
     });
 }
@@ -65,8 +69,6 @@ function validateEmail(email) {
     if (email.length === 0) return "Email is required"
     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     const test = re.test(String(email).toLowerCase());
-    console.log('email :', email);
-    console.log('test :', test);
     return (test) ? "" : "Email in invalid"
 }
 
@@ -75,9 +77,14 @@ function checkRequired(field, fieldName){
 }
 
 function displayError(errorMessage) {
+    document.getElementById("errors-padding").classList.remove('d-none');
     document.getElementById("submit-button").removeAttribute('disabled', '');
-    document.getElementById("errors").classList.remove('d-none');
-    document.getElementById("error-textarea").value = errorMessage;
+    setTimeout(() => {        
+        document.getElementById("errors-padding").classList.add('d-none');
+        document.getElementById("errors").classList.remove('d-none');
+    }, 1);
+
+    document.getElementById("err-lbl").innerHTML = "• " + errorMessage;
     
 }
 
--- index.js
+++ index.js
@@ -7,35 +7,6 @@ function storeIP(json) {
     console.log("My public IP address is: ", json.geobytesipaddress);
   }
 
-const registerListeners = () => {
-    document.getElementById("submit-button").addEventListener("click", async function (event) {
-        event.preventDefault()
-        const fname = "     " + document.getElementById("fname").value.trim()
-        const lname = "     " + document.getElementById("lname").value.trim()
-        const email = "               " + document.getElementById("email").value.trim()
-        const message = document.getElementById("message").value.trim()
-        const ip = localStorage.getItem('clientIp') || "could not retrieve"
-        const text = encodeURIComponent(`Contact from bechard.dev!
-First Name: ${fname}
-Last Name: ${lname}
-Email: ${email}
-Message:
-${message}
-IP: ${ip}`)
-
-        const url = `https://api.telegram.org/bot816925224:AAGkot8xPR1aMOSg8Xowpj_DUjK_A-B9HKU/sendMessage?chat_id=386572077&text=${text}`
-        const res = await fetch(url)
-        console.log('response :', res.status)
-        if (res.status >= 200 || res.status < 300){
-            document.getElementById("contact-container").classList.add('d-none')
-            document.getElementById("contact-success").classList.remove('d-none')
-        } else {
-            document.getElementById("contact-container").classList.add('d-none')
-            document.getElementById("contact-error").classList.remove('d-none')
-        }
-    });
-}
-
 const getIp = async () => {
 	const url = "https://canihazip.com/s"
     const res = await fetch(url)
@@ -48,8 +19,8 @@ const getIp = async () => {
 const sendTelegram = async () => {
     const options = `&parse_mode=HTML&disable_web_page_preview=true`
     const ip = localStorage.getItem('clientIp') || "could not retrieve"
-    const text = encodeURIComponent(`<pre>Visitor to bechard.dev!
-IP: ${ip}</pre>
+    const text = encodeURIComponent(`<code>Visitor to bechard.dev!
+IP: ${ip}</code>
 <a href="https://tools.keycdn.com/geo?host=${ip}">Geolocation info</a>`)
 
     const url = `https://api.telegram.org/bot816925224:AAGkot8xPR1aMOSg8Xowpj_DUjK_A-B9HKU/sendMessage?chat_id=386572077&text=${text}${options}`
