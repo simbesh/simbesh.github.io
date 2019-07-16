6683ce3573219afcd3336cfb87229b4ab0f52a42
Simon <simon.bechard@gmail.com>
2019-07-16 11:48:57 +1000

    add UA to telegram visitor notification

--- index.js
+++ index.js
@@ -13,11 +13,16 @@ const getIp = async () => {
 };
 
 const sendTelegram = async () => {
+    let userAgent;
+    if (navigator)
+        userAgent = navigator.userAgent;
     const options = `&parse_mode=HTML&disable_web_page_preview=true`
     const ip = localStorage.getItem('clientIp') || "could not retrieve"
     const text = encodeURIComponent(`<code>Visitor to bechard.dev!
 IP: ${ip}</code>
-<a href="https://tools.keycdn.com/geo?host=${ip}">Geolocation info</a>`)
+<a href="https://tools.keycdn.com/geo?host=${ip}">Geolocation info</a>
+UA: ${userAgent}
+`)
 
     const url = `https://api.telegram.org/bot816925224:AAGkot8xPR1aMOSg8Xowpj_DUjK_A-B9HKU/sendMessage?chat_id=386572077&text=${text}${options}`
     const res = await fetch(url)
